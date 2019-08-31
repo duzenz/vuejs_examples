8b7e5a2791ee77292b9d6b23a08291553511d94c
duzenz <duzenz@gmail.com>
2019-08-31 23:25:55 +0300

    component communication

---
+++ Section8- CommunicateBetweenComponents/src/components/UserDetail.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User name : {{ switchName() }}</p>
+    </div>
+</template>
+
+<script> 
+    export default {
+        props: {
+            myName: {
+                type: String,
+                required: true,
+                default : "zafer"
+            }
+        },
+        //props: ['name'],
+        methods: {
+            switchName: function() {
+                return this.name.split("").reverse().join("");
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
